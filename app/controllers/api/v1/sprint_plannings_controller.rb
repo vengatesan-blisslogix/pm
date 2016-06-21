6d2e7b948d1e7a5116bfe20ff59ed3f608fd28ab
yogeshblisslogix <yogeshblisslogix@gmail.com>
2016-06-21 16:49:02 +0530

    API changes in sprint

--- app/controllers/api/v1/sprint_plannings_controller.rb
+++ app/controllers/api/v1/sprint_plannings_controller.rb
@@ -100,7 +100,7 @@ puts "#{@search}"
 	def create
 	  @sprint_planning = SprintPlanning.new(sprint_params)
 	    if @sprint_planning.save
-        @sprint_planning.active = "active"
+        @sprint_planning.active = "1"
         @sprint_planning.save
        # SprintStatus.create(status: "active", active: 1, user_id: 1)
 		    render json: { valid: true, msg:"#{@sprint_planning.sprint_name} created successfully."}  
