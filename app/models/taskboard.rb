0782f97ba16515fcf8a7929e8054e44b8189e40a
yogeshblisslogix <yogeshblisslogix@gmail.com>
2016-08-02 16:29:47 +0530

    taskboard

--- app/models/taskboard.rb
+++ app/models/taskboard.rb
@@ -3,7 +3,7 @@ class Taskboard < ActiveRecord::Base
 #default_scope { order('created_at DESC') }
 
 
-default_scope { where(active: "active")}
+default_scope { where(status: "active")}
 
 	
 	belongs_to :project_master
