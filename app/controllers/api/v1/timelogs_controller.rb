1be6ac080d927f5b7630eb0da364f5b8450f4a85
yogeshblisslogix <yogeshblisslogix@gmail.com>
2016-08-02 15:27:07 +0530

    added time logs

--- app/controllers/api/v1/timelogs_controller.rb
+++ app/controllers/api/v1/timelogs_controller.rb
@@ -22,8 +22,12 @@ def create
     @timelog = Logtime.new(timelog_params)
     if @timelog.save
           @timelog.task_date = params[:date]
-          @timelog.task_time = ((@timelog.end_time  - @timelog.start_time) / 1.hour)
-
+          if params[:task_time] and params[:task_time]!=nil
+            @timelog.task_time = params[:task_time]
+          else
+            @timelog.task_time = ((@timelog.end_time  - @timelog.start_time) / 1.hour)
+          end  
+  
           @timelog.user_id = params[:user_id]
        @timelog.save
     	
