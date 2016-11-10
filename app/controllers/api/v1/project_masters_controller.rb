ef85aede97a6ffa2f342bbc7722f5af7311cdc69
yogeshblisslogix <yogeshblisslogix@gmail.com>
2016-11-10 11:44:44 +0530

    update with pm

--- app/controllers/api/v1/project_masters_controller.rb
+++ app/controllers/api/v1/project_masters_controller.rb
@@ -142,7 +142,8 @@ end
 
  def update   
 
-    if @project_master.update(project_master_params)  	      
+    if @project_master.update(project_master_params)  	
+    puts "---------#{@project_master.project_master_id}------------"      
        render json: { valid: true, msg:"#{@project_master.project_name} updated successfully."}
      else
         render json: { valid: false, error: @project_master.errors }, status: 404
