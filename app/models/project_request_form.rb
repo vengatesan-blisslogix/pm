3999586d106fd24e5522aa2582cde60b68f47605
yogeshblisslogix <yogeshblisslogix@gmail.com>
2016-12-21 18:57:43 +0530

    Updated with file

--- app/models/project_request_form.rb
+++ app/models/project_request_form.rb
@@ -1,10 +1,13 @@
 class ProjectRequestForm < ActiveRecord::Base
 
-	has_attached_file :signed_copy, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  	has_attached_file :mail_approval, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :signed_copy, styles: {  }, default_url: "/images/:style/missing.png"
+  	has_attached_file :mail_approval, styles: { }, default_url: "/images/:style/missing.png"
     
-    validates_attachment_content_type :signed_copy, content_type: /\Aimage\/.*\Z/
-  	validates_attachment_content_type :mail_approval, content_type: /\Aimage\/.*\Z/	
+    validates_attachment_content_type :signed_copy, :content_type  => ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",'application/pdf', /\Aimage\/.*\Z/, 
+    	 "application/pdf", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-powerpoint", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/zip","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    ]
+  	validates_attachment_content_type :mail_approval, :content_type  => ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",'application/pdf', /\Aimage\/.*\Z/, 
+  		  "application/pdf", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-powerpoint", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/zip","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"  	]	
   	validates_with AttachmentSizeValidator, attributes: :signed_copy,
                                          less_than: 2.megabytes
 
