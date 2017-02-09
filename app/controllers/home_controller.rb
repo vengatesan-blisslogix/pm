03b80006c0918c8b3779a7ce450fdb359210c3dc
yogeshblisslogix <yogeshblisslogix@gmail.com>
2017-02-09 13:18:06 +0530

    Updated with project_master_id

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1408,7 +1408,7 @@ end
                      'ProjectName' => @project_master.project_name,
                      'ProjectId'    => @project_master.id,
                      'Release'   => @resp_rel, 
-                     #'Sprint' => @resp_sprint
+                     'Sprint' => @resp_sprint
                     }
     end
     end
