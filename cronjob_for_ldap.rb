493ce1c16545cb3999b872263ad9a83e87c83fa5
yogeshblisslogix <yogeshblisslogix@gmail.com>
2017-02-17 15:53:54 +0530

    updated cronjobs with production db

--- cronjob_for_ldap.rb
+++ cronjob_for_ldap.rb
@@ -29,55 +29,50 @@ ActiveRecord::Base.establish_connection({
 
     result.each do |u|
        #p u['reporting_to']
-        if u['leftOrg'] == false and u['email'] != nil
-       		p u['email']
+        if u['leftOrg'] == false and u['email'] != nil       		
 	       @find_user = User.find_by_email(u['email'])
+	       p "-------if-----#{u['email']}"
 			if @find_user != nil
 			@user = User.find_by_email(u['email'])
 			#@user.nickname = u['name']+" "+"(#{u['empNo']})"
 			#@user.save #reporting_to run no 2
 				if @user!=nil and u['reportingTo'] != nil
 				@find_repo = CronReporting.where("reporting_name ='#{u['reportingTo']}'")
-			  	if @find_repo != nil and @find_repo.size!=0
-                  	@user.reporting_to_id= @find_repo[0].reporting_id
-                  	@user.reporting_to= @find_repo[0].reporting_name
-                  	@user.reporting_id= @find_repo[0].reporting_id.to_s+"|"+@find_repo[0].reporting_name
-                  	@user.save#reporting_to run no 2
-                  end
-                  
-			   
+				  	if @find_repo != nil and @find_repo.size!=0
+	                  	@user.reporting_to_id= @find_repo[0].reporting_id
+	                  	@user.reporting_to= @find_repo[0].reporting_name
+	                  	@user.reporting_id= @find_repo[0].reporting_id.to_s+"|"+@find_repo[0].reporting_name
+	                  	@user.save#reporting_to run no 2
+	                end		   
 				#@user.reporting_to = u['reportingTo']
 				@user.save
 				end
-		   else
-		   	p u['email'] u['empNo']
-		    @user = User.new
-	   		@user.employee_no 		= u['empNo']
-			@user.name 				= u['name'].split(" ")[0]
-			@user.last_name 		= u['name'].split(" ")[1]
-			@test_doj 				= Date.parse(u['doj'])
-			@user.doj         		= @test_doj.to_date
-			@user.dob         		= u['dob'].to_date
+		   	else
+			   	p "-------else-----#{u['email']}"
+			    @user = User.new
+		   		@user.employee_no 		= u['empNo']
+				@user.name 				= u['name'].split(" ")[0]
+				@user.last_name 		= u['name'].split(" ")[1]
+				@test_doj 				= Date.parse(u['doj'])
+				@user.doj         		= @test_doj.to_date
+				@user.dob         		= u['dob'].to_date
 
-			@user.prior_experience 	= u['previousExperience']
-			@user.email 			= u['email']
-			@user.password 			= u['email'][0..2]+"#123$"
-			@user.active 			= "active"
-			@user.mobile_no			= u['mobile']
-			@user.nickname = u['name']+" "+"(#{u['empNo']})"
-
-				if u['reportingTo'] != nil			 
-					@user.reporting_to		= u['reportingTo'].split("(")[0].strip
-				else
-					@user.reporting_to		= u['reportingTo']
-				end
-		    
-		    @user.uid 				= u['email']
-		    @user.branch_id 		= 1
-			@user.company_id 		= 1
-			@user.role_master_id 	= 2      
-		 
-	      @user.save
+				@user.prior_experience 	= u['previousExperience']
+				@user.email 			= u['email']
+				@user.password 			= u['email'][0..2]+"#123$"
+				@user.active 			= "active"
+				@user.mobile_no			= u['mobile']
+				@user.nickname = u['name']+" "+"(#{u['empNo']})"
+					if u['reportingTo'] != nil			 
+						@user.reporting_to		= u['reportingTo']
+					else
+						@user.reporting_to		= u['reportingTo']
+					end		    
+			    @user.uid 				= u['email']
+			    @user.branch_id 		= 1
+				@user.company_id 		= 1
+				@user.role_master_id 	= 2      			 
+	      	  @user.save
 		   end
 		else
 
