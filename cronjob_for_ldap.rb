4ef2580f8470d4eec914e71ea37ced22f2600bcb
yogeshblisslogix <yogeshblisslogix@gmail.com>
2017-02-17 16:56:11 +0530

    updated cronjobs with production db

--- cronjob_for_ldap.rb
+++ cronjob_for_ldap.rb
@@ -31,12 +31,13 @@ ActiveRecord::Base.establish_connection({
        #p u['reporting_to']
         if u['leftOrg'] == false and u['email'] != nil       		
 	       @find_user = User.find_by_email(u['email'])
+	       p "----------find user-------#{@find_user}"
 			if @find_user != nil
 			@user = User.find_by_email(u['email'])
 			#@user.nickname = u['name']+" "+"(#{u['empNo']})"
 			#@user.save #reporting_to run no 2
 			p "-------if-----#{u['reportingTo']}"
-				if @user!=nil					
+				if @user!=nil and u['reportingTo'] != nil					
 				@find_repo = CronReporting.where("reporting_name ='#{u['reportingTo']}'")
 				  	if @find_repo != nil and @find_repo.size!=0
 	                  	@user.reporting_to_id= @find_repo[0].reporting_id
