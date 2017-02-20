f9b9bb3d0ac62b6d1b9a43212e8b397616914ad5
yogeshblisslogix <yogeshblisslogix@gmail.com>
2017-02-20 16:00:25 +0530

    Added with mail

--- app/controllers/api/v1/project_users_controller.rb
+++ app/controllers/api/v1/project_users_controller.rb
@@ -296,9 +296,9 @@ end
       puts "--AAA-----------#{user}"
       if @user != nil and @mail_send.to_i==1
         if @manager[p].to_i  == 1
-          #UserNotifier.welcome_manager(@user.email, @user.name).deliver_now
+          UserNotifier.welcome_manager(@user.email, @user.name).deliver_now
         elsif @manager[p].to_i  == 0
-          #UserNotifier.welcome_user(@user.email, @user.name).deliver_now
+          UserNotifier.welcome_user(@user.email, @user.name).deliver_now
         end
       end                
 
@@ -397,9 +397,9 @@ end
         if @manager[p].to_i  == 1
           @mail_send_who << 1
           @mail_user << user
-          #UserNotifier.welcome_manager(@user.email, @user.name).deliver_now!
+          UserNotifier.welcome_manager(@user.email, @user.name).deliver_now!
         elsif @manager[p].to_i  == 0
-          #UserNotifier.welcome_user(@user.email, @user.name).deliver_now!
+          UserNotifier.welcome_user(@user.email, @user.name).deliver_now!
           @mail_send_who << 0
           @mail_user << user
         end
